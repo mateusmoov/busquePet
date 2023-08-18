ff65886c75341c22a2c42c1d02644ac79a7c3012
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-08-17 23:19:18 -0300

    feat: implement other variants and types

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,18 +1,26 @@
 import { ReactNode } from "react";
 
-interface ButtonProps { 
-  children: ReactNode
+type VariantProps = Record<string, string>
+type VariantKeyProps = keyof typeof variants
+
+interface ButtonProps {
+  children: ReactNode;
+  variant: VariantKeyProps;
 }
 
+const variants = {
+  filled: "rounded-lg bg-fuchsia-800 py-2 px-11 font-body flex",
+  outlined:
+    "rounded-lg outline outline-1 outline-fuchsia-800 py-2 px-11 font-body",
+  gradient:
+    "rounded-full bg-gradient-to-r from-purple-800 to-teal-500 py-2 px-6 font-body",
+} satisfies VariantProps;
 
-export const Button = ({ children }: ButtonProps ) => {
+export const Button = ({ children, variant }: ButtonProps ) => {
 
-  const variants = {
-    primary: ''
-  }
 
   return (
-    <button className='rounded-lg bg-fuchsia-800 py-2 px-11 font-body flex'>
+    <button className={variants[variant]}>
       {children}
     </button>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { Button, Icon } from './components'
 
 export default function Home() {
   return (
-      <Button>
-        Próximo
-        <Icon variant='arrowRight' />
+    <>
+      <Button variant="filled">Próximo</Button>
+      <Button variant="gradient">Quero adotar</Button>
+      <Button variant="outlined">
+        <span className="text-fuchsia-800">Próximo</span>
       </Button>
+    </>
   );
 }
