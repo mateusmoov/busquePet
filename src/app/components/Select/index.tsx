1e9d42be6d63022eb8d7cab23979b8764785deb2
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-08-20 22:59:13 -0300

    feat: finish select component

--- src/app/components/Select/index.tsx
+++ src/app/components/Select/index.tsx
@@ -1,20 +1,58 @@
-type SelectItemProps = {
-  value: string
-  name: string
-}
+import { twMerge } from "tailwind-merge";
 
-type SelectProps = {
-  label: string
-  selectItems: SelectItemProps[];
-  onChangeValue: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+type SelectItemProps = {
+  value: string;
+  name: string;
 };
 
+type SelectProps = {
+  label: string;
+  variant: VariantProps;
+  selectItems: SelectItemProps[];
+  onChangeValue?: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+};
 
-export const Select = ({label, selectItems, onChangeValue }: SelectProps) => {
+type VariantsProps = {
+  label: string;
+  select: string;
+};
+
+type VariantProps = keyof typeof variants;
+
+const variants = {
+  donor: {
+    label: "text-fuchsia-800",
+    select: "border-fuchsia-800",
+  },
+  adopter: {
+    label: "text-cyan-700",
+    select: "border-cyan-700",
+  },
+} satisfies Record<string, VariantsProps>;
+
+export const Select = ({
+  label,
+  variant,
+  selectItems,
+  onChangeValue,
+}: SelectProps) => {
   return (
     <>
-      <label htmlFor="">{label}</label>
-      <select name="select" onChange={onChangeValue}>
+      <label
+        className={twMerge(variants[variant].label, "block")}
+        htmlFor={label}
+      >
+        {label}
+      </label>
+      <select
+        id={label}
+        name="select"
+        onChange={onChangeValue}
+        className={twMerge(
+          variants[variant].select,
+          "text-black font-body py-3 rounded-xl border-2 block"
+        )}
+      >
         {selectItems.map((item, index: number) => (
           <option key={index} value={item.value}>
             {item.name}
--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -2,3 +2,4 @@ export { Button } from "./Button";
 export { Icon } from "./Icon";
 export { Header } from "./Header"
 export { Input } from './Input'
+export { Select } from './Select'
