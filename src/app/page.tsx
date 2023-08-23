82e83974cfdbb4b3257f3ea3a85500ad5d5ee173
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-08-22 21:55:21 -0300

    improve: remove import from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { RadioButton } from "./components";
 
 export default function Home() {
 
   return (
     <>
-    <RadioButton variant="adopter" label="Olá meus amigos" />
     </>
   );
 }
