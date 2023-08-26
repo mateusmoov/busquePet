0b419a60a173dcd7d3d86999f68a6373416d6dba
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-08-25 21:48:49 -0300

    feat: finish the signin page

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -3,29 +3,53 @@ import { Section, Button } from "../components";
 
 export const SignIn = () => {
   return (
-    <main>
+    <main className="flex items-center justify-center h-screen relative">
       <Section>
-        <h1>Cadastre-se</h1>
-        <div>
+        <h1 className="font-body text-4xl font-semibold">Cadastre-se</h1>
+        <div className="mb-10 flex flex-col items-center">
           <Image
             src="/cat_tea.svg"
             alt="Gato segurando uma chaleira"
             width="220"
             height="220"
           />
-          <h2>Como você quer se cadastrar?</h2>
+          <h2 className="font-body text-lg font-semibold mb-7">
+            Como você quer se cadastrar?
+          </h2>
         </div>
-        <div>
-          <Button variant="filled">
-            <span>Cadastrar como adotante</span>
-            <span>Quero um bicho para dar amor e um lar digno.</span>
+        <div className="flex flex-col gap-y-6 text-white">
+          <Button
+            variant="filled"
+            className="bg-fuchsia-800  px-5 py-4"
+            icon="arrowRight"
+          >
+            <div className="flex flex-col text-start">
+              <span className="mb-2 font-semibold">
+                Cadastrar como adotante
+              </span>
+              <span>Quero um bicho para dar amor e um lar digno.</span>
+            </div>
           </Button>
-          <Button variant="filled">
-            <span>Cadastrar-me como doador</span>
-            <span>Quero doar para que bichinho tenha um lar digno.</span>
+          <Button
+            variant="filled"
+            className="bg-cyan-700 px-5 py-4"
+            icon="arrowRight"
+          >
+            <div className="flex flex-col text-start">
+              <span className="mb-2 font-semibold">
+                Cadastrar-me como doador
+              </span>
+              <span>Quero doar para que bichinho tenha um lar digno.</span>
+            </div>
           </Button>
         </div>
       </Section>
+      <div className="absolute bottom-0 left-0 transform rotate-173.242 origin-bottom-left">
+        <Image src="/blob.svg" alt="bolha lateral" width="200" height="250" />
+      </div>
     </main>
   );
 };
+
+
+export default SignIn;
