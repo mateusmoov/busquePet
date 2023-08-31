e8c378fcbf1e2a689261f32d868edbc77762062a
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-08-30 22:06:41 -0300

    feat: add second pet register

---
+++ src/app/petcadastro2/page.tsx
@@ -0,0 +1,60 @@
+import { ageRanges } from "@/utils/age";
+import { Header, Button, Section, FormGroup, RadioButton, Select, Input } from "../components";
+const PetRegister2 = () => {
+  return (
+    <>
+      <Header variant="donor" icon>
+        <div className="flex flex-col gap-x-2 ml-7 py-4">
+          <span className="font-header text-white text-xl font-semibold">
+            Vamos ajudar o bichinho a encontrar um lar de amor?!
+          </span>
+        </div>
+      </Header>
+      <main>
+        <Section>
+          <div className="flex min-h-[100vh] flex-col gap-y-8">
+            <div className="mt-7">
+              <Input
+                label="Qual o nome do seu bichinho?"
+                type="text"
+                variant="adopter"
+              />
+            </div>
+            <FormGroup
+              label="O seu animalzinho já tem todas as vacinas?"
+              variant="adopter"
+              className="py-4"
+            >
+              <div className="flex flex-col gap-y-4">
+                <RadioButton
+                  label="Sim, todas as vacinas necessárias"
+                  value="todasVacinas"
+                />
+                <RadioButton
+                  label="Não, não tomou as vacinas"
+                  value="naoVacinas"
+                />
+                <RadioButton
+                  label="Apenas algumas vacinas"
+                  value="algumasVacinas"
+                />
+                <RadioButton label="Não sei" value="naosabe" />
+              </div>
+            </FormGroup>
+          </div>
+          <div className="flex justify-end mb-9">
+            <Button
+              variant="filled"
+              icon="arrowRight"
+              className="bg-cyan-700 flex px-14 py-3 items-center font-medium text-white"
+            >
+              Próximo
+            </Button>
+          </div>
+        </Section>
+      </main>
+    </>
+  );
+};
+
+export default PetRegister2;
