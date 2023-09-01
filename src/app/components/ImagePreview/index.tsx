85e6cd8bc7db873d07109796d87cf910796fad53
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-09-01 20:40:50 -0300

    feat: add close button on image preview

--- src/app/components/ImagePreview/index.tsx
+++ src/app/components/ImagePreview/index.tsx
@@ -2,7 +2,17 @@ import Image from "next/image"
 
  export const ImagePreview = () => {
   return (
-    <div className="bg-zinc-50 border border-dashed border-zinc-400 w-fit rounded">
+    <div className="bg-zinc-50 border border-dashed border-zinc-400 w-fit rounded relative">
+      <div className="absolute -top-3 -right-1">
+        <button>
+          <Image
+            src="/icons/close.svg"
+            alt="Apagar imagem"
+            width="10"
+            height="10"
+          />
+        </button>
+      </div>
       <div className="px-4 py-4">
         <Image
           src="/icons/image.svg"
