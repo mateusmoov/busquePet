5ebd20f94d8148b4adee1e41327b49fe19877105
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-09-01 21:06:31 -0300

    feat: add input file

--- src/app/components/ImagePreview/index.tsx
+++ src/app/components/ImagePreview/index.tsx
@@ -1,26 +1,38 @@
+"use client"
+
 import Image from "next/image"
 
  export const ImagePreview = () => {
   return (
-    <div className="bg-zinc-50 border border-dashed border-zinc-400 w-fit rounded relative">
-      <div className="absolute -top-3 -right-1">
-        <button>
+    <>
+      <input
+        type="file"
+        accept="image/*"
+        multiple
+        onChange={(e: any) =>
+          console.log(URL.createObjectURL(e?.target?.files[0]))
+        }
+      />
+      <div className="bg-zinc-50 border border-dashed border-zinc-400 w-fit rounded relative">
+        <div className="absolute -top-3 -right-1">
+          <button>
+            <Image
+              src="/icons/close.svg"
+              alt="Apagar imagem"
+              width="10"
+              height="10"
+            />
+          </button>
+        </div>
+        <div className="px-4 py-4">
           <Image
-            src="/icons/close.svg"
-            alt="Apagar imagem"
-            width="10"
-            height="10"
+            src="/icons/image.svg"
+            alt="Icone de pré visualização de imagem"
+            width="14"
+            height="14"
           />
-        </button>
+        </div>
       </div>
-      <div className="px-4 py-4">
-        <Image
-          src="/icons/image.svg"
-          alt="Icone de pré visualização de imagem"
-          width="14"
-          height="14"
-        />
-      </div>
-    </div>
+    </>
   );
 }
