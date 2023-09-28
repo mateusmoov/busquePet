d9d36328d8554a1504bc641bac186adfe27a0ce3
mateusmoov <mateusvasconcelos4210@gmail.com>
2023-09-27 22:48:18 -0300

    feat: finish the structure of petcard

--- src/app/components/PetCard/index.tsx
+++ src/app/components/PetCard/index.tsx
@@ -2,26 +2,27 @@ import Image from "next/image";
 
 export const PetCard = () => {
   return (
-    <div className="flex">
-      <div>
-        <Image
-          width="192"
-          height="136"
-          src="/cachorro.png"
-          alt="Pet"
-        />
-      </div>
-      <div className="w-44 h-32 gap-y-3 bg-red-900">
-        <div className="flex flex-col ml-4">
-          <span>Melzinha</span>
-          <span>1 - 3 meses</span>
+    <div className="flex w-full max-h-[136px]">
+      <Image
+        width="192"
+        height="136"
+        src="/cachorro.jpg"
+        alt="Pet"
+        className="rounded-l-lg"
+      />
+      <div className="w-full p-1 rounded-r-lg border border-neutral-200 border-l-0">
+        <div className="flex flex-col ml-4 gap-y-1 align-center">
+          <span className="font-body text-fuchsia-800 font-semibold">
+            Aquiles
+          </span>
+          <span className="font-body text-zinc-800">1 - 3 meses</span>
           <div className="flex justify-between items-center">
-            <Image width="49" height="22" src="/dog-m.png" alt="Pet" />
-            <Image width="30" height="30" src="/femea.png" alt="Pet" />
+            <Image width="49" height="22" src="/dog-g.svg" alt="Porte do Animal" />
+            <Image width="30" height="30" src="/macho.svg" alt="Genero do Animal" />
           </div>
-          <div className="flex">
-            <Image width="6" height="10" src="/place.png" alt="Pet" />
-            <span>Rio de Janeiro</span>
+          <div className="flex align-baseline">
+            <Image width="6" height="10" src="/place.svg" alt="Localização" className="mr-2" />
+            <span className="font-body text-xs">Riacho Fundo - Brasilia</span>
           </div>
         </div>
       </div>
---
+++ src/app/test-component-page/page.tsx
@@ -0,0 +1,11 @@
+import { PetCard, Section } from "../components"
+
+const TestComponentPage = () => {
+  return (
+    <Section className="pt-6">
+      <PetCard />
+    </Section>
+  );
+}
+
+export default TestComponentPage
